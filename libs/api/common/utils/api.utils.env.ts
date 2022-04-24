9f588a8b1e6db9f9aad628eab73089df4d524f0d
cool725 <109250241+cool725@users.noreply.github.com>
2022-04-24 08:42:46 +0200

    .env

--- libs/api/common/utils/api.utils.env.ts
+++ libs/api/common/utils/api.utils.env.ts
@@ -1,14 +1,14 @@
+import path from "path";
 export const env = (() => {
-  return {
-    load: (path = null) => {
-      if (!path) path = require('path').resolve(__dirname, '.env');
-      path && require('fs').existsSync(path)
-        ? require('dotenv').config({ path: require('path').resolve(path) })
-        : require('dotenv').config({
-            path: require('path').resolve(__dirname, '..', '.env'),
-          });
-
-      if (!process.env.APP_ENV) process.env.APP_ENV = 'development';
-    },
-  };
+    return {
+        load: (pathName = null) => {
+            if (!pathName) pathName = path.resolve(__dirname, '.env');
+            pathName && require('fs').existsSync(pathName)
+                ? require('dotenv').config({ path: path.resolve(pathName) })
+                : require('dotenv').config({
+                    path: path.resolve(__dirname, '..', '.env'),
+                });
+            if (!process.env.APP_ENV) process.env.APP_ENV = 'development';
+        },
+    };
 })();

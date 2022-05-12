e24741851c436af47a7394a465f2ad7499b4782f
cool725 <109250241+cool725@users.noreply.github.com>
2022-05-12 19:17:34 +0200

    locales preparation

--- apps/app/business/src/app/structure/pages/frontoffice/crm/profiles/form/profiles-form.module.ts
+++ apps/app/business/src/app/structure/pages/frontoffice/crm/profiles/form/profiles-form.module.ts
@@ -6,16 +6,18 @@ import { NgModule } from '@angular/core';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { ProfilesAPI } from '../packages/profile-api.service';
 import { environment } from '../../../../../../../environments/environment';
+import {MdbSharedModule} from "@movit/app/ui";
 
 @NgModule({
   declarations: [ProfilesFormComponent],
-  imports: [
-    CommonModule,
-    TranslateModule.forChild(),
-    FormsModule,
-    ReactiveFormsModule,
-    NzSelectModule,
-  ],
+    imports: [
+        CommonModule,
+        TranslateModule.forChild(),
+        FormsModule,
+        ReactiveFormsModule,
+        NzSelectModule,
+        MdbSharedModule,
+    ],
   providers: [
     ProfilesAPI,
     {

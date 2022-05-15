581fae40b2140c335dc7912092a8b347882e7d20
cool725 <109250241+cool725@users.noreply.github.com>
2022-05-15 17:41:21 +0200

    changes

--- libs/app/ui/mdb/mdb.module.ts
+++ libs/app/ui/mdb/mdb.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbTabsModule } from "mdb-angular-ui-kit/tabs";
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [MdbRippleModule, MdbFormsModule],
+  exports: [MdbRippleModule, MdbFormsModule, MdbTabsModule],
 })
 export class MdbSharedModule {}

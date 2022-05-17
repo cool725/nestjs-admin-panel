8b538d1d4ce01866e47db944895560cf68d95d75
cool725 <109250241+cool725@users.noreply.github.com>
2022-05-17 23:17:34 +0200

    anz-select animation

--- libs/app/ui/mdb/directives/mdb.nz-select.directive.ts
+++ libs/app/ui/mdb/directives/mdb.nz-select.directive.ts
@@ -1,28 +1,22 @@
 import { Directive, ElementRef, OnChanges, OnDestroy, OnInit, SimpleChanges } from "@angular/core";
 import { NzSelectComponent } from "ng-zorro-antd/select";
-import { Observable, Subject, takeUntil } from "rxjs";
+import { Subject, takeUntil } from "rxjs";
 
 @Directive({
   selector: '[mdbNzSelect]'
 })
 export class MdbNzSelectDirective implements OnInit, OnDestroy {
+
   onDispose = new Subject();
+
   node:HTMLElement;
+
   constructor(private nzS: NzSelectComponent) {}
+
   ngOnInit() {
     this.node = <HTMLElement>(this.nzS.originElement.nativeElement).parentElement;
     this.removeActive()
     this.listenForChanges()
-  }
-
-  listenForChanges(){
-     this.nzS['nzOpenChange']
-       .pipe(takeUntil(this.onDispose))
-       .subscribe(() => this.updateStates());
-     this.nzS['listOfValue$']
-       .pipe(takeUntil(this.onDispose))
-       .subscribe(()=> this.updateStates()
-    )
   }
 
   ngOnDestroy() {
@@ -30,18 +24,24 @@ export class MdbNzSelectDirective implements OnInit, OnDestroy {
     this.onDispose.complete();
   }
 
-  updateStates(){
+  listenForChanges(){
+    this.nzS['nzOpenChange']
+      .pipe(takeUntil(this.onDispose))
+      .subscribe(() => this.updateStates());
+    this.nzS['listOfValue$']
+      .pipe(takeUntil(this.onDispose))
+      .subscribe(()=> this.updateStates()
+      )
+  }
 
+  updateStates(){
     this.nzS.listOfValue.length ?
       this.node.classList.add('has-value') :
       this.node.classList.remove('has-value');
-
-    console.log(this.nzS.listOfValue.length,this.node.classList.contains('has-value'))
   }
 
   removeActive(){
     setTimeout(()=>{
-      console.log(this.node.classList,this.node)
       this.node.classList.remove("active")
     })
   }

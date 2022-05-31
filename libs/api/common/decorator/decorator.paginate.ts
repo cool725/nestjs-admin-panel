614f7cb621f4fb9f18bed73fe7ae066c1ad6e4bd
cool725 <109250241+cool725@users.noreply.github.com>
2022-05-31 12:54:46 +0200

    custom counting

--- libs/api/common/decorator/decorator.paginate.ts
+++ libs/api/common/decorator/decorator.paginate.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import {Like, Repository} from "typeorm";
+import { Like, Repository } from 'typeorm';
 
 export interface Pagination {
   page?: number;
@@ -7,117 +7,142 @@ export interface Pagination {
   limit?: number;
   sort?: { field: string; by: 'ASC' | 'DESC' }[];
   search?: { field: string; value: string }[];
-  apply<T>(repo:Repository<T>,companyId:number, options?:any):Promise<{ data: Repository<T>[], count: number , total: number, page: number }>
+  apply<T>(
+      repo: Repository<T>,
+      companyId: number,
+      options?: any
+  ): Promise<{
+    data: Repository<T>[];
+    count: number;
+    total: number;
+    page: number;
+  }>;
 }
 
-const  applyPagination = async <T>  (pagination:Pagination, repo:Repository<T>,companyId:number, options?:any):Promise<any> => {
+const applyPagination = async <T>(
+    pagination: Pagination,
+    repo: Repository<T>,
+    companyId: number,
+    options?: any
+): Promise<any> => {
   const where = [];
 
   // or
-  for(let search of pagination.search){
+  for (let search of pagination.search) {
     where.push({
-          companyId:companyId,
-          [search.field]:
-          search.field === 'phone' ?
-              Like('%'+search.value+'%') :
-                 Like(search.value+`%`)
-        })
+      companyId: companyId,
+      [search.field]:
+          search.field === 'phone'
+              ? Like('%' + search.value + '%')
+              : Like(search.value + `%`),
+    });
   }
 
   // and
+  // to implement
 
-  if(where.length === 0){
-    where.push({companyId})
+  // fallback
+  if (where.length === 0) {
+    where.push({ companyId });
   }
 
-  const [result, total] = await <any>repo.findAndCount({
-    where: where,
-    order: pagination.sort.reduce((order, sort) => ({ ...order, [sort.field]: sort.by }), {}),
-    skip: pagination.skip,
-    take: pagination.limit,
-  }).catch((e) => {
-    console.error(e);
-    return {
-      data:[]
-    }
-  });
+  const [result, total] = await Promise.all([
+    repo
+        .find({
+          where: where,
+          order: pagination.sort.reduce(
+              (order, sort) => ({ ...order, [sort.field]: sort.by }),
+              {}
+          ),
+          skip: pagination.skip,
+          take: pagination.limit,
+        })
+        .catch((e) => {
+          console.error(e);
+          return [];
+        }),
+    repo.count({
+      where: {
+        companyId: companyId,
+      },
+    }),
+  ]);
 
   return {
     page: pagination.skip,
     count: result.length,
     data: result,
-    total: total
-  }
-}
-
+    total: total,
+  };
+};
 
 export const GetPagination = createParamDecorator(
-  (data, ctx: ExecutionContext): Pagination => {
-    const req: any /*Request*/ = ctx.switchToHttp().getRequest();
-    const query = req['query'] || req.body;
-    const search = query.search || query.searchValue || query.searchTerm;
+    (data, ctx: ExecutionContext): Pagination => {
+      const req: any /*Request*/ = ctx.switchToHttp().getRequest();
+      const query = req['query'] || req.body;
+      const search = query.search || query.searchValue || query.searchTerm;
 
-    const paginationParams: Pagination = {
-      page: (query.page > 0 ? query.page : 1) || 1,
-      skip: 0,
-      limit: query.limit ? parseInt(query.limit) : 15,
-      sort: [],
-      search: [],
-      apply<T>(repo:Repository<T>,companyId:number,options:any){
-        return applyPagination(this,repo,companyId ,options)
+      const paginationParams: Pagination = {
+        page: (query.page > 0 ? query.page : 1) || 1,
+        skip: 0,
+        limit: query.limit ? parseInt(query.limit) : 15,
+        sort: [],
+        search: [],
+        apply<T>(repo: Repository<T>, companyId: number, options: any) {
+          return applyPagination(this, repo, companyId, options);
+        },
+      };
+
+      paginationParams.skip =
+          (paginationParams.page - 1) * paginationParams.limit;
+
+      // create array of sort
+      if (query.sort) {
+        const sortArray = query.sort.toString().split(',');
+        paginationParams.sort = sortArray.map((sortItem) => {
+          const sortBy = sortItem[0];
+          switch (sortBy) {
+            case '-':
+              return {
+                field: sortItem.slice(1),
+                by: 'ASC',
+              };
+            case '+':
+              return {
+                field: sortItem.slice(1),
+                by: 'ASC',
+              };
+            default:
+              return {
+                field: sortItem.trim(),
+                by: 'DESC',
+              };
+          }
+        });
       }
-    };
 
-    paginationParams.skip =
-      (paginationParams.page - 1) * paginationParams.limit;
+      // create array of search
+      if (search) {
+        // refactorize by
 
-    // create array of sort
-    if (query.sort) {
-      const sortArray = query.sort.toString().split(',');
-      paginationParams.sort = sortArray.map((sortItem) => {
-        const sortBy = sortItem[0];
-        switch (sortBy) {
-          case '-':
-            return {
-              field: sortItem.slice(1),
-              by: 'ASC',
-            };
-          case '+':
-            return {
-              field: sortItem.slice(1),
-              by: 'ASC',
-            };
-          default:
-            return {
-              field: sortItem.trim(),
-              by: 'DESC',
-            };
-        }
-      });
+        const searchArray = search.toString().split(',');
+        paginationParams.search = searchArray.map((searchItem) => {
+          let field = searchItem.split(':')[0];
+          let value = searchItem.split(':')[1];
+
+          if ((value === undefined && field) || field == 'all') {
+            value = value || field;
+            field = null;
+          }
+
+          return {
+            field,
+            value,
+          };
+        });
+        paginationParams.search = paginationParams.search.filter((a) => a.value);
+      }
+
+      return paginationParams;
     }
-
-    // create array of search
-    if (search) {
-      // refactorize by
-
-      const searchArray = search.toString().split(',');
-      paginationParams.search = searchArray.map((searchItem) => {
-        let field = searchItem.split(':')[0];
-        let value = searchItem.split(':')[1];
-
-        if ((value === undefined && field) || field == 'all') {
-          value = value || field;
-          field = null;
-        }
-
-        return {
-          field,
-          value,
-        };
-      });
-      paginationParams.search = paginationParams.search.filter(a => a.value);
-    }
-
-    return paginationParams;
-  }
 );

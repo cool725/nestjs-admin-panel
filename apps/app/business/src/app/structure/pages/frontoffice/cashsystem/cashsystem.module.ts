ef12c4d81061e255469606ad11374e4d46a08265
cool725 <109250241+cool725@users.noreply.github.com>
2022-06-08 20:05:41 +0200

    Revert "cashsystem changes"
    This reverts commit f012590990daf64f6a5a9e5cff2a2f8f3eec7293.

--- apps/app/business/src/app/structure/pages/frontoffice/cashsystem/cashsystem.module.ts
+++ apps/app/business/src/app/structure/pages/frontoffice/cashsystem/cashsystem.module.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CashSystemSettingsService } from "./packages/services/cashsystem.service-settings";
-import { CashSystemItemsService } from "./packages/services/cashsystem.service-items";
-import { CashSystemPaymentService } from "./packages/services/cashsystem.service-payment";
-
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers:[
-    CashSystemPaymentService,
-    CashSystemItemsService,
-    CashSystemSettingsService,
-  ]
 })
-export class CashSystemModule {}
+export class CashsystemModule {}
--- apps/app/business/src/app/structure/pages/frontoffice/cashsystem/packages/services/cashsystem.service-payment.ts
+++
@@ -1,5 +0,0 @@
-import {Injectable, NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@Injectable()
-export class CashSystemPaymentService {}

deb0e90e794c81aaf4483412f699c189abf78ea8
Pen Sokha <pichpen411@gmail.com>
2022-06-09 11:05:33 +0700

    comment socket module

--- apps/app/business/src/app/app.module.ts
+++ apps/app/business/src/app/app.module.ts
@@ -9,8 +9,8 @@ import {HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppHttpInterceptor } from '@movit/app/common';
 import { environment } from '../environments/environment';
 import { HTTPTranslateLoader, TranslateLocaleModule } from '@movit/app/module';
-import {SocketModule} from "../../../../../libs/app/common/services/socket/socket.module";
-import {SocketIoService} from "../../../../../libs/app/common/services/socket/adapters/socket-io.service";
+// import {SocketModule} from "../../../../../libs/app/common/services/socket/socket.module";
+// import {SocketIoService} from "../../../../../libs/app/common/services/socket/adapters/socket-io.service";
 
 export const getJtwToken = ():string =>{
   return 'test';
@@ -29,15 +29,15 @@ export const getJtwToken = ():string =>{
         useClass: HTTPTranslateLoader,
       },
     }),
-    SocketModule.forRoot({
-      config: {
-        jtwToken: getJtwToken,
-      },
-      transport:{
-        provide: 'socketAdapter',
-        useClass: SocketIoService,
-      }
-    })
+    // SocketModule.forRoot({
+    //   config: {
+    //     jtwToken: getJtwToken,
+    //   },
+    //   transport:{
+    //     provide: 'socketAdapter',
+    //     useClass: SocketIoService,
+    //   }
+    // })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AppHttpInterceptor, multi: true },

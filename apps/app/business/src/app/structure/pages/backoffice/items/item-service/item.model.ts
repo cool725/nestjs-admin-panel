2ebbd2182b1942a87068cac771255c78a8752ae8
Dang Nguyen <dang.nguyentienhai@gmail.com>
2022-06-13 22:33:31 +0200

    [MOVIT-43] - Service variant view improvements

--- apps/app/business/src/app/structure/pages/backoffice/items/item-service/item.model.ts
+++ apps/app/business/src/app/structure/pages/backoffice/items/item-service/item.model.ts
@@ -1,8 +1,7 @@
-import {Item, ItemVariant} from "../item.model";
+import { Item, ItemVariant } from '../item.model';
 
 export class ItemService extends Item {
-
-  override type = 'S'
+  override type = 'S';
 
   // create class for this
   override label = {
@@ -10,60 +9,65 @@ export class ItemService extends Item {
     desc: <any>{},
   };
 
-  override prices:ServiceVariant[] = [];
+  override prices: ServiceVariant[] = [];
 
-  employees:any = {}
+  employees: any = {};
 
   static create(item: Partial<Item>) {
     const newItem = new ItemService();
 
-    if(item.label){
-      newItem.label = Object.assign(
-          newItem.label, item.label
-      );
-      delete (<any>item).label
+    if (item.label) {
+      newItem.label = Object.assign(newItem.label, item.label);
+      delete (<any>item).label;
     }
 
-    if(item.prices){
-      item.prices.map(
-          (price => {
-        newItem.addVariant(price)
-      }))
+    if (item.prices) {
+      item.prices.map((price) => {
+        newItem.addVariant(price);
+      });
 
-      delete (<any>item).prices
+      delete (<any>item).prices;
     }
 
     return Object.assign(newItem, item);
   }
 
-  public removeVariant(index:number){
-    this.prices.splice(index,1);
+  public itemValid(): boolean {
+    return this.prices.every(
+      (price) => !!price.priceSell || price.priceSell === 0
+    );
   }
 
-  public addVariant(v?:Partial<ServiceVariant>){
+  public removeVariant(index: number) {
+    this.prices.splice(index, 1);
+  }
+
+  public addVariant(v?: Partial<ServiceVariant>) {
     this.prices.push(
-        ServiceVariant.create({
-          ... v || {},
-          type:this.type
-        })
-    )
+      ServiceVariant.create({
+        ...(v || {}),
+        type: this.type,
+      })
+    );
   }
 }
 
 export class ServiceVariant extends ItemVariant {
-  duration:Date;
-  bufferTimeStart:Date;
-  bufferTimeEnd:Date;
+  duration: number;
+  bufferTimeStart: number;
+  bufferTimeEnd: number;
+
   static override create(item?: Partial<ServiceVariant>) {
-    const newItem = new ServiceVariant()
-    if(item?.label){
-      for(const key in newItem.label){
+    const newItem = new ServiceVariant();
+    if (item?.label) {
+      for (const key in newItem.label) {
         newItem.label[key] = Object.assign(
-            newItem.label[key], item.label[key] || {}
+          newItem.label[key],
+          item.label[key] || {}
         );
       }
 
-      delete (<any>item).label
+      delete (<any>item).label;
     }
     return Object.assign(newItem, item || {});
   }
